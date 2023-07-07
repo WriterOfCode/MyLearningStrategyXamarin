bd7739810a1facbfbedf46a4fe293aa6b5cbb483
WriterOfCode <daviddavis@writerofcode.net>
2023-07-07 12:51:13 -0400

    Add files via upload

---
+++ MyLearningStrategyMobleXForms.ViewFlipper.iOS/ViewFlipperImplementation.cs
@@ -0,0 +1,20 @@
+﻿using MyLearningStrategyMobleXForms.ViewFlipper.Abstractions;
+using System;
+using Xamarin.Forms;
+using MyLearningStrategyMobleXForms.ViewFlipper.iOS;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(MyLearningStrategyMobleXForms.ViewFlipper.Abstractions.ViewFlipper), typeof(ViewFlipperRenderer))]
+namespace MyLearningStrategyMobleXForms.ViewFlipper.iOS
+{
+    /// <summary>
+    /// ViewFlipper Renderer
+    /// </summary>
+    public class ViewFlipperRenderer //: TRender (replace with renderer type
+    {
+        /// <summary>
+        /// Used for registration with dependency service
+        /// </summary>
+        public static void Init() { }
+    }
+}
