22af7e1902208a101c980a43b3dbc5da66d1adb0
WriterOfCode <daviddavis@writerofcode.net>
2023-07-07 13:04:24 -0400

    Add files via upload

---
+++ MyLearningStrategyMobleXForms/Behaviors/InvalidateEditorMissingText.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MyLearningStrategyMobleXForms.Behaviors
+{
+    public class InvalidateEditorMissingText : Behavior<Editor>
+    {
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        protected override void OnAttachedTo(Editor bindable)
+        {
+            bindable.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            Editor entry = sender as Editor;
+            entry.BackgroundColor = e.NewTextValue.Length == 0 ? Color.Red : Color.Default;
+
+
+        }
+    }
+}
---
+++ MyLearningStrategyMobleXForms/Behaviors/InvalidateEntryMissingText.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MyLearningStrategyMobleXForms.Behaviors
+{
+    class InvalidateEntryMissingText : Behavior<Entry>
+    {
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            Entry entry = sender as Entry;
+            entry.BackgroundColor = e.NewTextValue.Length == 0 ? Color.Red : Color.Default;
+        }
+    
+    }
+}
